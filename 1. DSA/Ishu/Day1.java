3d5f31571f4cd906fef68a130dd7d3a94aec6a75
ishusingh1401 <76910843+ishusingh1401@users.noreply.github.com>
2022-01-26 20:21:20 +0530

    Updated Changes

--- 1. DSA/Ishu/Day1.java
+++ 1. DSA/Ishu/Day1.java
@@ -1,10 +1,9 @@
 class Day1 {
-    public static void main (String args []) {
+    public static void main (String args[]) {
         System.out.println("Hello World");
     }
 }
 // Steps to Run this java file :-
-//Open this file in CMD or Terminal and run the following commands :-
 // javac Day1.java
 // java Day1
 // The output will be dispalyed.
