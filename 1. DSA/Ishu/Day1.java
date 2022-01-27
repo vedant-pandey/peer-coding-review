d3aefffcba90e4d9661eb8d904c88becd74a9212
ishusingh1401 <76910843+ishusingh1401@users.noreply.github.com>
2022-01-27 18:45:29 +0530

    Assignment Day1 By Ishu (#2)
    * Assignment Day1 By Ishu
    
    * Updated Changes

---
+++ 1. DSA/Ishu/Day1.java
@@ -0,0 +1,9 @@
+class Day1 {
+    public static void main (String args[]) {
+        System.out.println("Hello World");
+    }
+}
+// Steps to Run this java file :-
+// javac Day1.java
+// java Day1
+// The output will be dispalyed.
