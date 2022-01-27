5874134bdfb775e1dde8e6ec316526c83956f55d
vikasdwivedi957 <85984784+vikasdwivedi957@users.noreply.github.com>
2022-01-27 18:44:40 +0530

    Day 1 Assignment (#4)

---
+++ 1. DSA/Vikas/Day1.java
@@ -0,0 +1,27 @@
+
+/**
+ * @author vikas
+ * 
+ *         Steps to run the file :- javac Day1.java, java Day1 
+ *         Check the console for output
+ */
+public class Day1 {
+
+	/*
+	 * Alternative Method to print same output
+	 */
+	static {
+		System.out.println("Hello World !!");
+	}
+
+
+	/**
+	 * @param args - input the parameter
+	 * 
+	 * It will print the output in console.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World !!");
+	}
+
+}
